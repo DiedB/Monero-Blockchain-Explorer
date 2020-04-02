22b4c7b5992b85afb390d8218695446837a6960b
Diederik Bakker <diederik@outlook.com>
2020-04-02 17:42:04 +0200

    Implement TransactionNode styling

---
+++ src/Transaction/TransactionNode/TransactionNode.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import cx from 'classnames';
+
+import styles from './TransactionNode.module.css';
+
+const TransactionNode = ({ currentView, toggleView }) => (
+    <div className={cx([styles.TransactionNode, { [styles.Inter]: currentView, [styles.Intra]: !currentView }])} onClick={toggleView} />
+);
+
+export default TransactionNode;
