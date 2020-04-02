814575e77bd6b286b57d826f3345c38d918131fd
Thijs <thijs@MagicMikeXL.roaming.utwente.nl>
2020-04-03 00:02:15 +0200

    fixed chart date

--- src/Transaction/Chart/Chart.js
+++ src/Transaction/Chart/Chart.js
@@ -12,11 +12,11 @@ const Chart = ({ id }) => {
         const fetchTransactionHash = async (blockNumber, publicKey) => {
             const blockResult = await OnionApi.getBlock(blockNumber);
             const block = await blockResult.json();
-    
+
             for (let blockTransaction of block.data.txs) {
                 const transactionResult = await OnionApi.getTransaction(blockTransaction.tx_hash);
                 const transaction = await transactionResult.json();
-    
+
                 for (let transactionOutput of transaction.data.outputs) {
                     if (transactionOutput.public_key === publicKey) {
                         return blockTransaction.tx_hash
@@ -24,52 +24,56 @@ const Chart = ({ id }) => {
                 }
             }
         }
-    
+
         const fetchDatesForMark = async () => {
             const transactionList = [id];
             const temp_dates = [];
-    
+
             // Current timestamp
             while (true) {
                 const txId = transactionList.pop();
-    
+
                 const transactionResult = await OnionApi.getTransaction(txId);
                 const transaction = await transactionResult.json();
-    
+
                 temp_dates.push(transaction.data.timestamp_utc);
-    
-                for (let input of transaction.data.inputs) {
-                    for (let inputMixin of input.mixins) {
-                        const transactionHash = await fetchTransactionHash(inputMixin.block_no, inputMixin.public_key);
-                        transactionList.push(transactionHash);
-        
-                        const txResult = await OnionApi.getTransaction(transactionHash);
-                        const tx = await txResult.json();
-            
-                        temp_dates.push(tx.data.timestamp_utc);
-    
+
+                if (transaction.data.inputs) { // if statement added, no idea why but some are null (maybe mining rewards?)
+                    for (let input of transaction.data.inputs) {
+                        for (let inputMixin of input.mixins) {
+                            const transactionHash = await fetchTransactionHash(inputMixin.block_no, inputMixin.public_key);
+                            transactionList.push(transactionHash);
+
+                            const txResult = await OnionApi.getTransaction(transactionHash);
+                            const tx = await txResult.json();
+
+                            temp_dates.push(tx.data.timestamp_utc);
+
+                            if (temp_dates.length >= dateAmount) {
+                                break
+                            }
+                        }
                         if (temp_dates.length >= dateAmount) {
                             break
                         }
                     }
-                    if (temp_dates.length >= dateAmount) {
-                        break
-                    }
                 }
-    
+
                 if (temp_dates.length >= dateAmount) {
                     setDates(temp_dates);
                     setIsLoading(false);
-    
+
                     break
                 }
             }
         }
-    
-        fetchDatesForMark();    
+
+        fetchDatesForMark();
     }, [id]);
 
-    return isLoading ? "Loading..." : dates;
+    return isLoading ? "Loading..." : (
+        dates
+    );
 };
 
 export default Chart;
