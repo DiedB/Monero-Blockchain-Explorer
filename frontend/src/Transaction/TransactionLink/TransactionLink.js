4a470018968525b11e8b34f6cf0eff5ec410fc2c
DaWe <willteam35@gmail.com>
2024-02-17 23:02:58 +0800

    revert rm txHash

--- frontend/src/Transaction/TransactionLink/TransactionLink.js
+++ frontend/src/Transaction/TransactionLink/TransactionLink.js
@@ -2,9 +2,8 @@ import { Link } from 'react-router-dom';
 
 import styles from './TransactionLink.module.css';
 
-const TransactionLink = ({ blockInfo }) => {
-
-    return <Link className={styles.TransactionLink} to={`/transaction/${blockInfo.tx_hash}`} />
+const TransactionLink = ({ blockInfo, txHash }) => {
+    return <Link className={styles.TransactionLink} to={`/transaction/${txHash ?? blockInfo.tx_hash}`} />
 }
 
 export default TransactionLink;
